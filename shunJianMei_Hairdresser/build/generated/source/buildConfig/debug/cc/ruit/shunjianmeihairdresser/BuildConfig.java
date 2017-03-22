dfcdec996ec36ef0e7c7761b5742a58d5a4dbecd
BlackFox <huangy_tao@126.com>
2017-03-23 00:38:26 +0800

    将ShunJianMeiei_Hairdresser导入到AS中

---
+++ shunJianMei_Hairdresser/build/generated/source/buildConfig/debug/cc/ruit/shunjianmeihairdresser/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cc.ruit.shunjianmeihairdresser;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "cc.ruit.shunjianmeihairdresser";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.21";
+}
